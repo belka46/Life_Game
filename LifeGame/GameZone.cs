e58c146b7b25899d0198c6b3bfc2ffcf0874572d
Orlovsky_Oleg <oorlovsky86@gmail.com>
2018-07-14 15:29:20 +0300

    добавлен .gitignore

---
+++ LifeGame/GameZone.cs
@@ -0,0 +1,15 @@
+﻿namespace LifeGame
+{
+    /// <summary>
+    /// Создает игровое поле с заданными параметрами.
+    /// </summary>
+    internal class GameZone
+    {
+        private byte[,] gameZone { get; }
+
+        public GameZone(ulong x, ulong y)
+        {
+            gameZone = new byte[x, y];
+        }
+    }
+}
